6ae965e76c62c2a541cedf45ca4b024d0bdd5706
Russell Salacup <ta20eo@brocku.ca>
2025-02-14 13:15:55 -0500

    Added logo.

--- coursemix/src/components/Footer.jsx
+++ coursemix/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <div>
-            <h3 className="text-white font-semibold mb-4">Course Mix</h3>
+            <img src="/CourseMixLogo.png" />
           </div>
           <div>
             <h3 className="text-white font-semibold mb-4">Quick Links</h3>
