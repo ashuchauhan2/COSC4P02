52c2e7d79c98482244ba780b18f1ea16150432ac
Jerome Uwaneme <118835052+CJuwaneme1704@users.noreply.github.com>
2025-03-02 22:09:00 -0500

    Placeholder

---
+++ coursemix/app/(user-pages)/protected/course-reviews/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CourseReviewsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Course Reviews</h1>
+      <p>This is a placeholder for the course reviews page.</p>
+    </div>
+  );
+};
+
+export default CourseReviewsPage;
